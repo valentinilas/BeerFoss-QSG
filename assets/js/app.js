802733064457270caf18207b450f922d41bc2ca8
Valentin Ilas <valentin.ilas@gmail.com>
2020-09-07 19:49:13 +0200

    added cookie notice

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,101 +1,109 @@
 if (!Element.prototype.matches) {
-  Element.prototype.matches = Element.prototype.msMatchesSelector ||
-                              Element.prototype.webkitMatchesSelector;
+    Element.prototype.matches = Element.prototype.msMatchesSelector ||
+        Element.prototype.webkitMatchesSelector;
 }
 
 if (!Element.prototype.closest) {
-  Element.prototype.closest = function(s) {
-    var el = this;
+    Element.prototype.closest = function(s) {
+        var el = this;
 
-    do {
-      if (el.matches(s)) return el;
-      el = el.parentElement || el.parentNode;
-    } while (el !== null && el.nodeType === 1);
-    return null;
-  };
+        do {
+            if (el.matches(s)) return el;
+            el = el.parentElement || el.parentNode;
+        } while (el !== null && el.nodeType === 1);
+        return null;
+    };
 }
 
 
 // slick slider
 var slickOptions = {
-  dots: false,
-  speed: 500,
-  infinite:false,
-  slidesToShow: 1,
-  lazyLoad: 'progressive',
-  adaptiveHeight: true,
-  prevArrow: document.querySelector('.btn-back'),
-  nextArrow: document.querySelector('.btn-next')
+    dots: false,
+    speed: 500,
+    infinite: false,
+    slidesToShow: 1,
+    lazyLoad: 'progressive',
+    adaptiveHeight: true,
+    prevArrow: document.querySelector('.btn-back'),
+    nextArrow: document.querySelector('.btn-next')
 
 };
 // update progress
 var $progressBar = $('.progress__inner');
-var $progressBarLabel = $( '.slider__label' );
+var $progressBarLabel = $('.slider__label');
 
-$('.slide-container').on('afterChange init', function(event, slick, currentSlide, nextSlide){
+$('.slide-container').on('afterChange init', function(event, slick, currentSlide, nextSlide) {
 
-  var slideCount = slick.slideCount;
-  var slideCurrent = slick.currentSlide;
-  var calc = ( (slideCurrent) / (slick.slideCount - 1) ) * 100;
+    var slideCount = slick.slideCount;
+    var slideCurrent = slick.currentSlide;
+    var calc = ((slideCurrent) / (slick.slideCount - 1)) * 100;
 
-  $progressBar.css('width', calc + '%');
+    $progressBar.css('width', calc + '%');
 
-  $('.slide-current-number').text(slideCurrent + 1);
-  $('.slide-total-number').text(slideCount);
-
-
-
-  if((slideCurrent + 1) == slideCount){
-    //hide next button at the end
-    $('.btn-next').addClass('disabled');
-  }
-  else{
-    $('.btn-next').removeClass('disabled');
-  }
-  if(slideCurrent == 0){
-    $('.btn-back').addClass('disabled');
-  }
-  else{
-    $('.btn-back').removeClass('disabled');
-  }
-
-
-});
+    $('.slide-current-number').text(slideCurrent + 1);
+    $('.slide-total-number').text(slideCount);
 
 
 
-
-$(document).on("click",function(e){
-  var clickTarget = e.target;
-
-  // modal
-  if(clickTarget.closest("#help-button")){
-    $('#modal-help').addClass('active');
-
-  }
-  if(clickTarget.closest(".youtube-link")){
-    var yt = $(clickTarget).attr('data-yt');
-
-    $('#modal-youtube').find('iframe').attr('src','//www.youtube.com/embed/'+yt+'?controls=0');
-    $('#modal-youtube').addClass('active');
-
-  }
-  if(clickTarget.closest('.close-modal')){
-    $('#modal-youtube').find('iframe').attr('src','');
-      $('.modal').removeClass('active');
-  }
-
-  // progress
-  if(clickTarget.closest('.initialize-app')){
-    $('#app').addClass('initialized');
-    if(!$('.slide-container').hasClass('slick-initialized')){
-      $('.slide-container').slick(slickOptions);
+    if ((slideCurrent + 1) == slideCount) {
+        //hide next button at the end
+        $('.btn-next').addClass('disabled');
+    } else {
+        $('.btn-next').removeClass('disabled');
     }
-    $('.slide-container').slick('slickGoTo', 0);
-    $('#modal-infobox').addClass('active');
+    if (slideCurrent == 0) {
+        $('.btn-back').addClass('disabled');
+    } else {
+        $('.btn-back').removeClass('disabled');
+    }
 
-  }
-  if(clickTarget.closest('.foss-logo')){
-    $('#app').removeClass('initialized')
-  }
+
 });
+
+
+
+
+$(document).on("click", function(e) {
+    var clickTarget = e.target;
+
+    // modal
+    if (clickTarget.closest("#help-button")) {
+        $('#modal-help').addClass('active');
+
+    }
+    if (clickTarget.closest(".youtube-link")) {
+        var yt = $(clickTarget).attr('data-yt');
+
+        $('#modal-youtube').find('iframe').attr('src', '//www.youtube.com/embed/' + yt + '?controls=0');
+        $('#modal-youtube').addClass('active');
+
+    }
+    if (clickTarget.closest('.close-modal')) {
+        $('#modal-youtube').find('iframe').attr('src', '');
+        $('.modal').removeClass('active');
+    }
+
+    // progress
+    if (clickTarget.closest('.initialize-app')) {
+        $('#app').addClass('initialized');
+        if (!$('.slide-container').hasClass('slick-initialized')) {
+            $('.slide-container').slick(slickOptions);
+        }
+        $('.slide-container').slick('slickGoTo', 0);
+        $('#modal-infobox').addClass('active');
+
+    }
+    if (clickTarget.closest('.foss-logo')) {
+        $('#app').removeClass('initialized')
+    }
+
+    // cookie notice close
+    if (clickTarget.closest('.btn--accept')) {
+        $('.cookie-notice').addClass('hidden');
+        sessionStorage.setItem('foss-cookie-notice', 'hidden');
+    }
+});
+
+if (sessionStorage.getItem('foss-cookie-notice') === 'hidden') {
+    $('.cookie-notice').addClass('hidden');
+}
